bad6f921d0ecbcaf0f2053fefd005c7784b1959b
damidaramola <damidaramola7@gmail.com>
2022-12-12 08:58:33 +0000

    add instructions to game

--- run.py
+++ run.py
@@ -5,3 +5,11 @@ add_username = input('What is your name?: ')
 print('Hello ' + add_username + '!')
 
 play_game = input('Would you like to play? ')
+
+print('''Instructions of the game
+ You are playing Earth, Fire, Water against the computer 
+ Type in either Earth, Fire or Water
+ Rules
+ 1.Earth Absorbs Water
+ 2.Water Quenches Fire
+ 3.Fire Burns Earth''')
