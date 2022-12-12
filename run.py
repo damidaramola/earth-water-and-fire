7db6c4d98f03f2ee06426048d9f50369551e8820
damidaramola <damidaramola7@gmail.com>
2022-12-12 00:09:09 +0000

    add code asking player if they want to play

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import random
 
-print('Welcome to our game - Earth, Fire, Water!')
+print('Welcome to - Earth, Fire, Water!')
 add_username = input('What is your name?: ')
 print('Hello ' + add_username + '!')
 
+play_game = input('Would you like to play? ')
