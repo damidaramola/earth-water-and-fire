244a875bf3153a4fadb674bfa17e373e79ab0048
damidaramola <damidaramola7@gmail.com>
2022-12-13 00:49:46 +0000

    fix syntax and white-spacing issues

--- run.py
+++ run.py
@@ -21,34 +21,36 @@ Instructions of the game
     2.Water Quenches Fire
     3.Fire Burns Earth ''')
 
-#Add variables to track score 
-you_win = 0 
+# Add variables to track score
+you_win = 0
 computer_wins = 0
-draw = 0 
+draw = 0
 select_option = ['Earth', 'Fire', 'Water']
 
-def check_winner(your_choice,computer_choice):
+
+def check_winner(your_choice, computer_choice):
     """
-    this function checks if you or computer wins 
+    this function checks if you or computer wins
     """
-    if(your_choice == 'Water' and computer_choice == 'Earth'):
+    if (your_choice == 'Water' and computer_choice == 'Earth'):
         print('You lost - Earth absorbs Water!')
         return 'computer'
-    elif(your_choice == 'Water' and computer_choice == 'Fire'):
-        print('You won! - Water quenches Fire!') 
-        return 'player'  
-    elif(your_choice == 'Fire' and computer_choice == 'Water'):
-        print('You lost  - Water quenches Fire!') 
-        return 'computer'  
-    elif(your_choice == 'Fire' and computer_choice == 'Earth'):
-        print('You won! - Fire Burns Earth!') 
-        return 'player'     
-    elif(your_choice == 'Earth' and computer_choice == 'Water'):
-        print('You won! - Earth absorbs Water!') 
+    elif (your_choice == 'Water' and computer_choice == 'Fire'):
+        print('You won! - Water quenches Fire!')
         return 'player'
-    elif(your_choice == 'Earth' and computer_choice == 'Fire'):
-        print('You lost - Fire Burns Earth!') 
+    elif (your_choice == 'Fire' and computer_choice == 'Water'):
+        print('You lost  - Water quenches Fire!')
+        return 'computer'
+    elif (your_choice == 'Fire' and computer_choice == 'Earth'):
+        print('You won! - Fire Burns Earth!')
+        return 'player'
+    elif (your_choice == 'Earth' and computer_choice == 'Water'):
+        print('You won! - Earth absorbs Water!')
+        return 'player'
+    elif (your_choice == 'Earth' and computer_choice == 'Fire'):
+        print('You lost - Fire Burns Earth!')
         return 'computer'
     else:
         print('It is a draw!, play again')
-        return 'draw'    
+        return 'draw'
+        
